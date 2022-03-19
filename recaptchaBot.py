139be55a3b21a413a6aaa0365f2b9f68dd4abb8f
Owais Quadri Home PC <owais.quadri@ontariotechu.net>
2022-03-18 20:40:46 -0400

    bot barebones

--- recaptchaBot.py
+++ recaptchaBot.py
@@ -1,1 +1,18 @@
 #bot to test custom recaptcha
+
+from selenium import webdriver as d
+
+DRIVER_PATH = "chromedriver.exe"
+
+driver = d.Chrome(DRIVER_PATH)
+link="http://localhost:3000/"
+link="http://google.ca"
+driver.get(link)
+
+print(driver.title)
+
+#current tab
+#driver.close()
+#quit browser
+driver.quit()
+
