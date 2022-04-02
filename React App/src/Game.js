ae03fd2d7ee4abdd188e3fdd7a83882851b4e8c8
Austin <austincolepage@hotmail.com>
2022-04-01 20:37:58 -0400

    updated react code

--- React App/src/Game.js
+++ React App/src/Game.js
@@ -1,31 +1,74 @@
 import { useEffect, useState } from "react";
+import target_top from "./target_top.png";
+import target_right from "./target_right.png";
+import target_left from "./target_left.png";
+import target_bottom from "./target_bottom.png";
 
 function Game() {
     const [targetPos, setTargetPos] = useState({ x: 10, y: 20 });
-    const [targetCounter, setTargetCounter] = useState(0);
+    const [targetCounter, setTargetCounter] = useState(1);
     const [showThing, setShowThing] = useState(true);
+    const [showOtherThing, setShowOtherThing] = useState(false);
+    const images = [target_top, target_left, target_right, target_bottom];
+    const [image, setImage] = useState(target_top);
 
     const handleClick = (e) => {
+        console.log(targetCounter);
+        console.log(image);
         let element = document.getElementById("dot");
         var rect = element.getBoundingClientRect();
         var mouseX = e.clientX;
         var mouseY = e.clientY;
-        var midY = (rect.bottom - rect.top) / 2 + rect.top;
-        var midX = (rect.right - rect.left) / 2 + rect.left;
-        console.log(midX - mouseX, midY - mouseY);
-        setTargetPos({ x: Math.random() * 450, y: Math.random() * 450 });
-        setTargetCounter(targetCounter + 1);
-        if (targetCounter > 5) {
-            setShowThing(false);
+
+        if (image === target_top) {
+            if (mouseY <= rect.top + 12.5) {
+                console.log("good");
+            } else {
+                console.log("bad");
+            }
+        } else if (image === target_bottom) {
+            if (mouseY >= rect.bottom - 12.5) {
+                console.log("good");
+            } else {
+                console.log("bad");
+            }
+        } else if (image === target_left) {
+            if (mouseX <= rect.left + 12.5) {
+                console.log("good");
+            } else {
+                console.log("bad");
+            }
+        } else if (image === target_right) {
+            if (mouseX >= rect.right - 12.5) {
+                console.log("good");
+            } else {
+                console.log("bad");
+            }
         }
-        console.log(targetCounter);
+
+        if (targetCounter > 4) {
+            setShowThing(false);
+            setShowOtherThing(true);
+        }
+
+        setTargetPos({ x: Math.random() * 450, y: Math.random() * 450 });
+        setImage(images[Math.floor(Math.random() * images.length)]);
+        setTargetCounter(targetCounter + 1);
     };
 
     useEffect(() => {
-        let element = document.getElementById("dot");
-        element.addEventListener("mousedown", handleClick);
-        return () => {};
-    }, []);
+        let element = null;
+        if (showThing) {
+            element = document.getElementById("dot");
+            element.addEventListener("mousedown", handleClick);
+        }
+
+        return () => {
+            if (showThing) {
+                element.removeEventListener("mousedown", handleClick);
+            }
+        };
+    }, [targetCounter]);
 
     return (
         <div
@@ -45,13 +88,15 @@ function Game() {
                         position: "relative",
                         top: targetPos.y,
                         left: targetPos.x,
-                        backgroundColor: "red",
                         width: 50,
                         height: 50,
                         borderRadius: 999,
+                        backgroundImage: `url(${image})`,
+                        backgroundSize: "cover",
                     }}
                 ></div>
             )}
+            {showOtherThing && <p>aa</p>}
         </div>
     );
 }
