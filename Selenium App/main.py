ae45508c44b0db14ba553a5f13d07bbbae8e3c21
Owais Quadri <owais.quadri@ontariotechu.net>
2022-04-06 10:07:16 -0400

    add sleeps for presentation purposes

--- Selenium App/main.py
+++ Selenium App/main.py
@@ -31,6 +31,7 @@ print("Website Title:"+driver.title)
 submit_button=driver.find_element(By.ID,"submit")
 #press button
 submit_button.click()
+time.sleep(3)
 #click on the ___ target
 prompt=driver.find_element(By.ID,"prompt")
 colour=prompt.text.split()[3]
@@ -38,20 +39,26 @@ colour=prompt.text.split()[3]
 # check which target is coloured in that way
 target=driver.find_element(By.ID,colour)
 target.click()
+time.sleep(3)
 #press to fill progress bar
 fill_button=driver.find_element(By.ID,"bttn")
 done_button=driver.find_element(By.ID,"doneBtn")
 for i in range(4):
+    time.sleep(1)
     fill_button.click()
+time.sleep(1)
 done_button.click()
+time.sleep(3)
 # question about image
 # random selection from answer
 answers=[3,4,4,7]
 guess = random.choice(answers)
 text_input=driver.find_element(By.ID,"filled-basic")
 text_input.send_keys(guess)
+time.sleep(2)
 done_button=driver.find_element(By.ID,"done")
 done_button.click()
+time.sleep(3)
 #done
 
 
